650a94d331f77b5603e5f78f2f893c2f8f776141
Dom Slee <domslee1@gmail.com>
2023-06-03 23:29:35 +1000

    init

---
+++ Forceops/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ForceOps
+{
+    public class Program
+    {
+        static void Main()
+        {
+            ForceOpsMethods.MoveFileOrFolder(@"C:\Users\user\Documents\hi.txt", @"C:\Users\user\Documents\hi_moved.txt");
+        }
+    }
+}
