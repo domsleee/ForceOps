92bae31913542794f8e1dc1c1b78b760765ce7ef
github-action-benchmark <github@users.noreply.github.com>
2023-07-11 14:42:08 +0000

    add ForceOps Benchmarks (benchmarkdotnet) benchmark result for 07ee247454cacf4671efb105fbbf934c7e264cad

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1689085237205,
+  "lastUpdate": 1689086528029,
   "repoUrl": "https://github.com/domsleee/ForceOps",
   "entries": {
     "ForceOps Benchmarks": [
@@ -48,6 +48,54 @@ window.BENCHMARK_DATA = {
             "value": 190569078.57142857,
             "unit": "ns",
             "range": "± 2548218.3963819393"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "domslee1@gmail.com",
+            "name": "Dom Slee",
+            "username": "domsleee"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "07ee247454cacf4671efb105fbbf934c7e264cad",
+          "message": "Improve performance by ~30% by reducing calls to win32 api (#15)",
+          "timestamp": "2023-07-12T00:37:34+10:00",
+          "tree_id": "417c81d9436f74c5bd8ef3efda72a48a1677d586",
+          "url": "https://github.com/domsleee/ForceOps/commit/07ee247454cacf4671efb105fbbf934c7e264cad"
+        },
+        "date": 1689086520390,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "ForceOps.Benchmarks.FileAndDirectoryDeleterBenchmark.Deleter(NumFiles: 1000, FileSize: 10, IsInsideDirectory: False)",
+            "value": 113045704.05405405,
+            "unit": "ns",
+            "range": "± 3767077.1966985078"
+          },
+          {
+            "name": "ForceOps.Benchmarks.FileAndDirectoryDeleterBenchmark.DirectoryDelete(NumFiles: 1000, FileSize: 10, IsInsideDirectory: False)",
+            "value": 113418913.51351352,
+            "unit": "ns",
+            "range": "± 3817585.1878847126"
+          },
+          {
+            "name": "ForceOps.Benchmarks.FileAndDirectoryDeleterBenchmark.Deleter(NumFiles: 1000, FileSize: 10, IsInsideDirectory: True)",
+            "value": 201830266.66666666,
+            "unit": "ns",
+            "range": "± 3698765.4334608335"
+          },
+          {
+            "name": "ForceOps.Benchmarks.FileAndDirectoryDeleterBenchmark.DirectoryDelete(NumFiles: 1000, FileSize: 10, IsInsideDirectory: True)",
+            "value": 199957711.7647059,
+            "unit": "ns",
+            "range": "± 3907444.5133428243"
           }
         ]
       }
