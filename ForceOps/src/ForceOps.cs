7eb61378d417f15021c0318490bbb23752fa391e
Dom Slee <domslee1@gmail.com>
2023-12-06 19:57:49 +1100

    chore(release): 1.3.1 (#28)
    * Fix `forceops delete -h` help command

--- ForceOps/src/ForceOps.cs
+++ ForceOps/src/ForceOps.cs
@@ -63,8 +63,8 @@ internal class ForceOps
 
 		var forceOption = new Option<bool>(new[] { "-f", "--force" }, "Ignore nonexistent files and arguments.");
 		var disableElevate = new Option<bool>(new[] { "-e", "--disable-elevate" }, "Do not attempt to elevate if the file can't be deleted.");
-		var retryDelay = new Option<int>(new[] { "-d", "--retry-delay" }, () => 500, "Delay when retrying to delete a file, after deleting processes holding a lock. (default: 500)");
-		var maxRetries = new Option<int>(new[] { "-n", "--max-retries" }, () => 5, "Number of retries when deleting a locked file. (default: 5)");
+		var retryDelay = new Option<int>(new[] { "-d", "--retry-delay" }, () => 500, "Delay when retrying to delete a file, after deleting processes holding a lock.");
+		var maxRetries = new Option<int>(new[] { "-n", "--max-retries" }, () => 5, "Number of retries when deleting a locked file.");
 
 		var deleteCommand = new Command("delete", "Delete files or a directories recursively.")
 		{
