f4fd6112d0ec517a331f2d887b4ec9c972c490e7
Dom Slee <domslee1@gmail.com>
2024-05-31 19:52:41 +1000

    Add ForceOps.Lib example (ForceOps.DeleteExample) and docs to README.md

---
+++ ForceOps.Lib/src/ForceOpsContext/RelaunchHelpers.cs
@@ -0,0 +1,46 @@
+﻿using Serilog;
+
+namespace ForceOps.Lib;
+
+public static class RelaunchHelpers
+{
+	public static void RunWithRelaunchAsElevated(Action action, Func<List<string>> buildArgsForRelaunch, ForceOpsContext forceOpsContext, ILogger? logger = null, bool disableElevate = false)
+	{
+		logger = logger ?? forceOpsContext.loggerFactory.CreateLogger<object>();
+		try
+		{
+			action();
+		}
+		catch (Exception ex) when (IsExceptionCausedByPermissions(ex) && !forceOpsContext.elevateUtils.IsProcessElevated() && !disableElevate)
+		{
+			var args = buildArgsForRelaunch();
+			var childOutputFile = GetChildOutputFile();
+			args.AddRange(new[] { "2>&1", ">", childOutputFile });
+			logger.Information($"Unable to perform operation as an unelevated process. Retrying as elevated and logging to \"{childOutputFile}\".");
+			var childProcessExitCode = forceOpsContext.relaunchAsElevated.RelaunchAsElevated(args, childOutputFile);
+			if (childProcessExitCode != 0)
+			{
+				throw new AggregateException($"Child process failed with exit code {childProcessExitCode}.");
+			}
+			else
+			{
+				logger.Information("Successfully deleted as admin");
+			}
+		}
+	}
+
+	static string GetChildOutputFile()
+	{
+		return Path.GetTempFileName();
+	}
+
+
+	static bool IsExceptionCausedByPermissions(Exception ex)
+	{
+		if (ex is FileNotFoundException)
+		{
+			return false;
+		}
+		return ex is IOException || ex is UnauthorizedAccessException;
+	}
+}
---
+++ ForceOps.Test/src/DeleteExampleTest.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+using static ForceOps.Test.TestUtil;
+
+namespace ForceOps.Test;
+
+public class DeleteExampleTest
+{
+	[Fact]
+	public void DeleteExampleWorks()
+	{
+		var tempFilePath = GetTemporaryFileName();
+		using var launchedProcess = HoldLockOnFileUsingPowershell(tempFilePath);
+		var process = Process.Start(new ProcessStartInfo
+		{
+			FileName = GetDeleteExampleExePath(),
+			Arguments = tempFilePath,
+			RedirectStandardOutput = true
+		})!;
+
+		var standardOutput = process.StandardOutput.ReadToEnd();
+		process.WaitForExit();
+		Assert.Equal(0, process.ExitCode);
+		Assert.False(File.Exists(tempFilePath));
+		Assert.Contains("INF] Could not delete file", standardOutput);
+	}
+
+	private static string GetDeleteExampleExePath()
+	{
+		var currentAssemblyPath = Assembly.GetExecutingAssembly().Location;
+		var currentDirectory = Path.GetDirectoryName(currentAssemblyPath)!;
+		var deleteExampleExe = Path.Combine(currentDirectory.Replace("ForceOps.Test", "ForceOps.DeleteExample"), "ForceOps.DeleteExample.exe");
+
+		if (!File.Exists(deleteExampleExe))
+		{
+			throw new FileNotFoundException($"{deleteExampleExe} did not exist.");
+		}
+
+		return deleteExampleExe;
+	}
+}
