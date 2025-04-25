42d35492b48a2199d5816119f0eb27b5b14d7549
Dom Slee <domslee1@gmail.com>
2025-04-25 21:05:50 +1000

    Close

---
+++ ForceOps/src/ForceOps.cs
@@ -0,0 +1,52 @@
+﻿using ForceOps.Lib;
+using Serilog;
+
+namespace ForceOps;
+
+internal class ForceOps
+{
+	readonly internal ForceOpsContext forceOpsContext = new();
+	readonly ILogger logger;
+	internal Exception? caughtException = null;
+	internal List<string>? extraRelaunchArgs = null;
+
+	public ForceOps(ForceOpsContext? forceOpsContext = null, ILogger? logger = null)
+	{
+		this.forceOpsContext = forceOpsContext ?? new ForceOpsContext();
+		this.logger = logger ?? this.forceOpsContext.loggerFactory.CreateLogger<ForceOps>();
+	}
+
+	public void DeleteCommand(string[] args, string[] filesOrDirectoriesToDelete, bool force, bool disableElevate, int retryDelay, int maxRetries)
+	{
+		RelaunchHelpers.RunWithRelaunchAsElevated(() =>
+		{
+			forceOpsContext.maxRetries = maxRetries;
+			forceOpsContext.retryDelay = TimeSpan.FromMilliseconds(retryDelay);
+			var deleter = new FileAndDirectoryDeleter(forceOpsContext);
+			filesOrDirectoriesToDelete = filesOrDirectoriesToDelete.Select(file => DirectoryUtils.CombineWithCWDAndGetAbsolutePath(file)).ToArray();
+			foreach (var file in filesOrDirectoriesToDelete)
+			{
+				deleter.DeleteFileOrDirectory(file, force);
+			}
+		}, () => BuildArgsForRelaunch(args), forceOpsContext, logger, disableElevate);
+	}
+
+	public void ListCommand(string fileOrDirectory)
+	{
+		new ListFileOrDirectoryLocks(forceOpsContext).PrintLocks(fileOrDirectory);
+	}
+
+	List<string> BuildArgsForRelaunch(string[] args)
+	{
+		var newArgs = args.ToList();
+		if (!newArgs.Any(arg => arg == "-f" || arg == "--force"))
+		{
+			newArgs.Add("-f");
+		}
+		if (extraRelaunchArgs != null)
+		{
+			newArgs.AddRange(extraRelaunchArgs);
+		}
+		return newArgs;
+	}
+}
--- ForceOps/src/Program.cs
+++ ForceOps/src/Program.cs
@@ -6,9 +6,35 @@ namespace ForceOps;
 [SupportedOSPlatform("windows")]
 public class Program
 {
+	internal static string[] args;
+	internal static ForceOps forceops;
+
 	public static int Main(string[] args)
 	{
-		ConsoleApp.Run(args, (string message) => Console.Write($"Hello, {message}"));
+		Program.args = args;
+		forceops = new ForceOps();
+
+		var app = ConsoleApp.Create();
+		app.Add<MyCommands>();
+		app.Run(args);
+
 		return 0;
+	}
+}
+
+public class MyCommands
+{
+	/// <summary>
+	/// Delete files or a directories recursively.
+	/// </summary>
+	/// <param name="files">Files or directories to delete.</param>
+	/// <param name="force">-f, Ignore nonexistent files and arguments.</param>
+	/// <param name="disableElevate">-e, Do not attempt to elevate if the file can't be deleted.</param>
+	/// <param name="retryDelay">-d, Delay when retrying to delete a file, after deleting processes holding a lock.</param>
+	/// <param name="maxRetries">-n, Number of retries when deleting a locked file.</param>
+	[Command("delete")]
+	public void Delete([Argument] string[] files, bool force = false, bool disableElevate = false, int retryDelay = 50, int maxRetries = 10)
+	{
+		Program.forceops.DeleteCommand(files, files, force, disableElevate, retryDelay, maxRetries);
 	}
 }
